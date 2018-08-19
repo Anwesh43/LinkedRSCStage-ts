800f05f2dc049015b2d50839ae7dab38b8f198d9
Anwesh43 <anweshthecool0@gmail.com>
2018-08-19 19:08:39 +0530

    adding LinkedRSC class

--- LinkedRSCStage.ts
+++ LinkedRSCStage.ts
@@ -129,3 +129,25 @@ class RSCNode {
         return this
     }
 }
+
+class LinkedRSC {
+    private curr : RSCNode = new RSCNode(0)
+    private dir : number = 1
+
+    draw(context : CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    }
+
+    update(cb : Function) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb : Function) {
+        this.curr.startUpdating(cb)
+    }
+}
